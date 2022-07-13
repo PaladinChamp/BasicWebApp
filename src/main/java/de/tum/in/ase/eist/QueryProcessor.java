f730dbc28bd905b72bb0eee9cfdc92d4c642f70e
Fabian <ge63fob@mytum.de>
2022-07-13 11:06:39 +0200

    plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,11 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Fabi S";
-        } else { // TODO extend the programm here
+        } else if (query.contains("plus")){ // TODO extend the programm here
+            String[] arr =  query.split(" ");
+            return String.valueOf(Integer.parseInt(arr[2]) + Integer.parseInt(arr[4]));
+        }
+        else{
             return "";
         }
     }
